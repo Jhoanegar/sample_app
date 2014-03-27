56fe3cd5d6b140c28f3e8f641dae7f5d41ca6f16
Jhoan <jhoanegar@gmail.com>
2014-03-27 04:12:16 -0600

    Add Sign out option

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,7 +41,12 @@ describe "User pages" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success',
                                   text: 'Welcome') }
+        context 'followed by sign out' do
+          before { click_link "Sign out" }
+          it { should have_link "Sign in" }
+        end
       end
+
     end
   end
  
